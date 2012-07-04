4b095fb75a33631c29d0d28cb09d9ca0f474a7e5
cclamb <chrislambistan@gmail.com>
2012-07-04 09:16:56 -0600

    work

--- lib/s_3.rb
+++ lib/s_3.rb
@@ -57,6 +57,7 @@ module Logging::Appenders
       time = Time.now
       obj = @bucket.objects["#{@base_tag} #{time.inspect} - #{time.usec}".to_sym]
       obj.write "#{msg}source: #{@src_name}"
+      puts obj.to_s
     end
 
   end
