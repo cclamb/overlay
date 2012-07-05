e5836e457cd1f5ff09df084992568c55d72b299d
cclamb <cclamb@cs.unm.edu>
2012-07-05 06:33:41 -0600

    fleshed out some scenarios and added some step definitions

--- test/features/start.feature
+++ test/features/start.feature
@@ -3,7 +3,21 @@ Feature: Starting an overlay network
   In typical usage we need to be able to start a given overlay network from a centralized location.  We should be able to do this without a centralized log, but we should always have a configured system log.
 
   Scenario: Network already running
+  	When I start a network
+  	But a network is already running
+  	Then the running network will stop
+  	And the new network will start
+
   Scenario: Bad network configuration
+  	When I start a network
+  	But the network configuration is malformed
+  	Then the network will not start
+  	And I will be notified
+
   Scenario: No centralized log destination specified
+  	When I start a network
+  	And the centralized log is not specified
+  	Then the network will start without a centralized collector
+  	
   Scenario: Hierarchical network
   Scenario: Non-hierarchical network
---
+++ test/features/step_definitions/overlay_steps.rb
@@ -0,0 +1,36 @@
+
+When /^I start a network$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^a network is already running$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the running network will stop$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the new network will start$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^the network configuration is malformed$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the network will not start$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I will be notified$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^the centralized log is not specified$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the network will start without a centralized collector$/ do
+  pending # express the regexp above with the code you wish you had
+end
