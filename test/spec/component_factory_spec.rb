c3021cf04db95a71ab5a883fc94c320801d8f119
cclamb <cclamb@cs.unm.edu>
2012-07-10 05:22:11 -0600

    work

--- test/spec/component_factory_spec.rb
+++ test/spec/component_factory_spec.rb
@@ -3,8 +3,16 @@ require 'rspec'
 require_relative '../../lib/component_factory'
 
 describe ComponentFactory do
-    it 'should be creatable'
-    it 'should fail fast with incorrect parameters'
+
+    it 'should be creatable' do
+      factory = ComponentFactory.new :bucket_name => 'test'
+      factory.should_not eq nil
+    end
+
+    it 'should fail fast with incorrect parameters' do
+      factory = ComponentFactory.new :bucket_name => 'test'
+    end
+    
     context 'with a LogFactory' do
         it 'should create a system log'
         it 'should create an overlay log'
