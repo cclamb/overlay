645fc4c5bd7e06e20b8c8efba4bc045e609b416d
cclamb <chrislambistan@gmail.com>
2012-08-08 19:44:02 -0600

    work

--- lib/garden/domain/node.rb
+++ lib/garden/domain/node.rb
@@ -1,3 +1,19 @@
 class Garden::Domain::Node
 
+  def initialize args
+    cnt = args.keys.count { |x| x == :repository || x == :umm }
+    raise 'must include a :repository, :umm, and :dispacher' unless cnt == 2
+    @repository = args[:repository]
+    @context_factory = args[:context_factory]
+    @umm = args[:umm]
+  end
+
+  def artifact subject, device, key
+
+  end
+
+  def artifacts subject, device
+
+  end
+
 end
