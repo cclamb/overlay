3340d841f805d515db0c4352dd4bb0dd31516a76
cclamb <cclamb@cs.unm.edu>
2012-08-11 15:08:42 -0600

    work

--- lib/garden/domain/node.rb
+++ lib/garden/domain/node.rb
@@ -9,11 +9,19 @@ class Garden::Domain::Node
   end
 
   def artifact subject, device, key
-
+    artifact_description = repository.artifact k
+    return nil if artifact_description == nil
+    ctx = @context_factory.assemble_context subject, artifact_description, device
+    bundles = @rectifier.partition artifact_description
+    bundles_to_return = []
+    bundles.each do |bundle|
+      bundles_to_return.push bundle if umm.execute? bundle.policy, ctx, :read
+    end
+    @rectifier.assemble bundles_to_return
   end
 
   def artifacts subject, device
-
+    respository.artifacts
   end
 
 end
--- lib/garden/domain/router.rb
+++ lib/garden/domain/router.rb
@@ -16,28 +16,11 @@ class Garden::Domain::Router
   end
 
   def artifact subject, device, key
-    artifact_description = repository.artifact k
-    return nil if ad == nil
-    ctx = @context_factory.assemble_context subject, artifact_description, device
-    bundles = @rectifier.partition ad
-    bundles_to_return = []
-    bundles.each do |bundle|
-      bundles_to_return.push bundle if umm.execute? bundle.policy, ctx, :read
-    end
-    @rectifier.assemble bundles_to_return
-  end
 
-# ad = r.get_artifact(k)
-# return nil if ad == nil
-# ctx = cf.assemble_context(s, r, d)
-# cbl = ar.partition(ad)
-# for_each cb in cbl :
-#   push(cbr, cb) if umm.can_execute(cb.policy, ctx, activity.read)
-# adr = ar.assemble(ctr)
-# return adr    
+  end 
 
   def artifacts subject, device
-
+    @dispatcher.dispatch subject, device
   end
 
 end
