0342bf38260678d5b77e866b63168a39155c6e41
cclamb <chrislambistan@gmail.com>
2012-08-12 17:26:34 -0600

    dispatcher integration tests in place

--- integration/spec/dispatcher_spec.rb
+++ integration/spec/dispatcher_spec.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require 'stringio'
 
 require_relative '../../lib/garden/domain'
 require_relative '../../lib/garden/util/test_interface'
@@ -7,17 +8,12 @@ include Garden::Domain
 
 pid = nil
 
-
-
 class TestService < TestInterface
-
-
-
 	get '/artifacts/*' do
-
+    'retrieved artifacts'
 	end
 	get '/artifact/*' do
-
+    'retrieved an artifact'
 	end
 end
 
@@ -25,14 +21,16 @@ describe Dispatcher do
 
   before(:all) do
     Domain::ComponentFactory::instance :bucket_name => 'foo'
-    pid = fork { TestInterface::run! }
-    puts "PID: #{pid}"
+    pid = fork do
+      $stdout = StringIO.new
+      $stderr = StringIO.new
+      TestService::run!
+    end
     sleep 1
   end
 
   after(:all) do
   	File.delete('system.log') if File.exists?('system.log')
-  	puts "Killing process with PID: #{pid}"
   	Process::kill :KILL, pid
   end
 
@@ -40,8 +38,16 @@ describe Dispatcher do
     d = Dispatcher.new [], 4567
   end
 
-  it 'should dispatch to indicated hosts via HTTP' do
+  it 'should dispatch to indicated hosts via HTTP on artifacts requests' do
+    d = Dispatcher.new ['http://localhost'], 4567
+    responses = d.dispatch_artifacts 'cclamb', 'iphone'
+    responses.each { |r| r.body.should eq 'retrieved artifacts' }
+  end
 
+  it 'shjould dispatch to indicated hosts via HTTP on single artifact requests' do
+    d = Dispatcher.new ['http://localhost'], 4567
+    responses = d.dispatch_artifact 'cclamb', 'iphone', 'some_id'
+    responses.each { |r| r.body.should eq 'retrieved an artifact' }
   end
 
 end
--- lib/garden/domain/dispatcher.rb
+++ lib/garden/domain/dispatcher.rb
@@ -10,28 +10,43 @@ class Garden::Domain::Dispatcher
   end
 
   def dispatch_artifacts subject, device
-      request = params[:request]
+      # request = params[:request]
       responses = []
 
       @nodes.each do |node|
         uri_string = "#{node}:#{@port}/artifacts/#{subject}/#{device}"
-        @@syslog.info "submitting to node: #{uri_string}"
+        @syslog.info "submitting to node: #{uri_string}"
         uri = URI.parse uri_string
-        begin
-          http = Net::HTTP.new uri.host, uri.port
-          request = Net::HTTP::Get.new uri.request_uri, \
-            'X-Overlay-Port' => "#{@port}", 'X-Overlay-Role' => 'router'
-          response = http.request request
-          responses.push response if response.code == '200'
-        rescue RuntimeError => err
-          @@syslog.error "error thrown in router: #{err}"
-        end
+        response = send_request uri 
+        responses.push response if response.code == '200'
       end
       return responses    
   end
 
-  def dispatch_artifact
+  def dispatch_artifact subject, device, id
+      responses = []
+      @nodes.each do |node|
+        uri_string = "#{node}:#{@port}/artifact/#{subject}/#{device}/#{id}"
+        @syslog.info "submitting to node: #{uri_string}"
+        uri = URI.parse uri_string
+        response = send_request uri 
+        responses.push response if response.code == '200'
+      end
+      return responses   
+  end
 
+  private
+
+  def send_request uri
+    response = nil
+    begin
+      http = Net::HTTP.new uri.host, uri.port
+      request = Net::HTTP::Get.new uri.request_uri, \
+        'X-Overlay-Port' => "#{@port}", 'X-Overlay-Role' => 'router'
+      response = http.request request
+    rescue RuntimeError => err
+      @syslog.error "error thrown in router: #{err}"
+    end
   end
 
 end
