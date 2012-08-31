0d50a1bbb27fea067c545218ec172d48baec7dd9
cclamb <cclamb@cs.unm.edu>
2012-08-31 06:09:11 -0600

    policy work

---
+++ etc/policy-test.rb
@@ -0,0 +1,8 @@
+{
+	:mission_affiliation 		=> [:tropic_thunder],
+	:sensitivity				=> :top_secret,
+	:category					=> [:vermillion],
+	:organizational_affiliation	=> [:oceania],
+	:device						=> [:workstation, :phone],
+	:operating_system			=> [:unix, :macos]
+}
