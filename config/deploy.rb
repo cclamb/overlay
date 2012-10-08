6e845823cf32e6899d9162377e895291684f1893
cclamb <cclamb@cs.unm.edu>
2012-10-08 05:25:03 -0600

    renamed hierarchy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -64,7 +64,7 @@ role :router, '198.101.205.153'
 # config_file_name = 'etc/config.yaml'
 # config_file_name = 'etc/simple_hierarchy_config.yaml'
 #config_file_name = 'etc/all_hierarchy_config.yaml'
-config_file_name = 'etc/2_4_hierarchy_config.yaml'
+config_file_name = 'etc/1_2_hierarchy_config.yaml'
 
 access_key = creds['amazon']['access_key']
 secret_key = creds['amazon']['secret_key']
