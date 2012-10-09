676b83eaf795962d29fb3be2c04c02d8322dce8f
cclamb <cclamb@cs.unm.edu>
2012-10-09 06:31:39 -0600

    work

--- lib/garden/domain/dispatcher.rb
+++ lib/garden/domain/dispatcher.rb
@@ -18,7 +18,7 @@ class Garden::Domain::Dispatcher
       @nodes.each do |node|
         next if visited_nodes.include? node
         uri_string = "#{node}:#{@port}/search/artifacts/#{subject}/#{device}"
-        @syslog.info "submitting to node: #{uri_string}"
+        # @syslog.info "submitting to node: #{uri_string}"
         uri = URI.parse uri_string
         response = send_request uri, visited_nodes
         responses.push response.body if response.code == '200'
@@ -26,9 +26,9 @@ class Garden::Domain::Dispatcher
         #   keys = response.body[1..-1].chop.split ", "
         #   responses.concat keys
         # end
-        @syslog.info "single response is: #{response.body}"
+        #@syslog.info "single response is: #{response.body}"
       end
-      @syslog.info "responses are: #{responses}"
+      #@syslog.info "responses are: #{responses}"
       return responses    
   end
 
@@ -39,7 +39,7 @@ class Garden::Domain::Dispatcher
       @nodes.each do |node|
         next if visited_nodes.include? node
         uri_string = "#{node}:#{@port}/search/artifact/#{subject}/#{device}/#{id}"
-        @syslog.info "submitting to node: #{uri_string}"
+        #@syslog.info "submitting to node: #{uri_string}"
         uri = URI.parse uri_string
         response = send_request uri, visited_nodes
         responses.push response.body if response.code == '200'
