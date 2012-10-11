5ddcf430415e600298346bdb65c76d0de95a764d
cclamb <cclamb@cs.unm.edu>
2012-10-11 07:03:30 -0600

    work

--- lib/garden/domain/dispatcher.rb
+++ lib/garden/domain/dispatcher.rb
@@ -43,11 +43,11 @@ class Garden::Domain::Dispatcher
         @syslog.info "submitting to node: #{uri_string}"
         uri = URI.parse uri_string
         response = send_request uri, visited_nodes
-        @syslog.info response.body if response.code == '200'
+        @syslog.info "Body is: #{response.body}" if response.code == '200'
         responses.push response.body if response.code == '200'
         visited_nodes.push node
       end
-      @syslog.info responses
+      @syslog.info "Responses are: #{responses}"
       return responses   
   end
 
