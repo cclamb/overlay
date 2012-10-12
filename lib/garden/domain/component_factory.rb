75ef471342022444c144d33eca5a233207e1c5ec
cclamb <chrislambistan@gmail.com>
2012-10-12 14:11:49 -0600

    work

--- lib/garden/domain/component_factory.rb
+++ lib/garden/domain/component_factory.rb
@@ -29,7 +29,6 @@ class Garden::Domain::ComponentFactory
   def initialize values
     @log_factory = LogFactory.new values[:bucket_name]
     @node_record_factory = NodeRecordFactory.new
-    @syslog = Domain::ComponentFactory::instance.create_system_log self
   end
 
   # Creating and returning an instance of the class.
@@ -90,7 +89,8 @@ class Garden::Domain::ComponentFactory
 
   # Create a usage manager.
   def create_rectifier args = { :confidentiality_strategy => :redact, :managed => false }
-    @syslog.info ":managed is #{args[:managed]}"
+    syslog = Domain::ComponentFactory::instance.create_system_log self
+    syslog.info ":managed is #{args[:managed]}"
     if args[:managed] == true
       Util::ContentRectifier.new \
         :umm => Domain::UsageManagementMechanism.new,
