874a5af1b64a1407a05e66279d62e8f4760283af
cclamb <chrislambistan@gmail.com>
2012-10-12 13:04:02 -0600

    work

--- lib/garden/util/content_rectifier.rb
+++ lib/garden/util/content_rectifier.rb
@@ -4,21 +4,16 @@ class Garden::Util::ContentRectifier
 
   def initialize params
     @umm = params[:umm]
-    @context_mgr = params[:context_manager]
     @syslog = Domain::ComponentFactory::instance.create_system_log self
     @strategy = params[:confidentiality_strategy] || :redact
   end
 
   def process args
-    #components = Util::parse_response args[:artifact]
-    #return components[:artifact] if components[:policy] == nil
-
     doc = Nokogiri::XML args[:artifact]
     policy_set = doc.xpath '//artifact/policy-set'
 
     return args[:artifact] if policy_set == nil
 
-    #data_object = doc.xpath '//artifact/data-object'
     sections = doc.xpath '//artifact/data-object/content/section'
 
     evaluator = Util::PolicyEvaluator.new do
@@ -27,17 +22,8 @@ class Garden::Util::ContentRectifier
 
     sections.each do |section|
       policy_name = section.attr 'policy'
-      puts "PN: #{policy_name}"
-      #puts check_set(failed_sections, policy_name)
-      section.remove if @umm.execute? \
-      evaluator.ctx[policy_name.to_sym], \
-      @context_mgr.context, \
-      :transmit # check_set(failed_sections, policy_name)
+      section.remove unless @umm.execute? evaluator.ctx[policy_name.to_sym], args[:context], :transmit
     end
-
-
-    #TODO: you *must* reform the results into a valid document to be passed on!
-    #args[:artifact]
     doc.to_s
   end
 
--- test/spec/util/content_rectifier_spec.rb
+++ test/spec/util/content_rectifier_spec.rb
@@ -9,64 +9,67 @@ describe Util::ContentRectifier do
   old_detail = File.read "#{File.dirname __FILE__}/../../../etc/demo/old-location-detail.xml"
 
 	context 'with a nil rectifier' do
+
     it 'should pass data through untouched' do
       Util::NilContentRectifier.new.process(:artifact => new_detail).should eq new_detail
     end
+
   end
+
   context 'with an artifact containing a policy' do
 
     context 'with a redaction strategy' do
 
-      it 'should redact indicated content - sensitivity' do
-        class Foo
-          def context
-            {
-              :link => {
-                :sensitivity => :secret,
-                :category => :magenta,
-                :organization => :eurasia,
-                :mission_affiliation => :tropic_thunder
-              },
-              :user => {
-                :clearance => :top_secret,
-                :category => :magenta,
-                :organziation => :oceania,
-                :mission_affiliation => :tropic_thunder,
-                :device => :tablet
-              }
-            }
-          end
-        end
-        ctx_mgr = Foo.new
+      it 'should redact indicated content (secret)' do
+        context = {
+          :sensitivity => :secret,
+          :category => :magenta,
+          :organization => :eurasia,
+          :mission_affiliation => :tropic_thunder
+        }
+
         cr = Util::ContentRectifier.new \
           :umm => Domain::UsageManagementMechanism.new,
-          :context_manager => ctx_mgr,
           :confidentality_strategy => :redact
-        xml = cr.process :artifact => new_detail
-        puts xml
+        xml = cr.process :artifact => new_detail, :context => context
+
+        doc = Nokogiri::XML xml
+        sections = doc.xpath '//artifact/data-object/content/section'
+        sections.each do |section|
+          policy_name = section.attr 'policy'
+          fail unless policy_name == 'description'
+        end
       end
 
-      it 'should redact indicated content - category'
-      it 'should redact indicated content - mission affiliation'
-      it 'should redact indicated content - organization'
-      it 'should redact indicated content - device'
-      it 'should redact indicated content - os'
+      it 'should redact indicated content (magenta)' do
+        context = {
+          :sensitivity => :top_secret,
+          :category => :magenta,
+          :organization => :eurasia,
+          :mission_affiliation => :tropic_thunder
+        }
+
+        cr = Util::ContentRectifier.new \
+          :umm => Domain::UsageManagementMechanism.new,
+          :confidentality_strategy => :redact
+        xml = cr.process :artifact => new_detail, :context => context
+        
+        doc = Nokogiri::XML xml
+        sections = doc.xpath '//artifact/data-object/content/section'
+        sections.each do |section|
+          policy_name = section.attr 'policy'
+          fail unless policy_name == 'description' || policy_name == 'history'
+        end
+      end
+
     end
     context 'with an encryption strategy' do
-      # it 'should ecrypt indicated content - sensitivity'
-      # it 'should ecrypt indicated content - category'
-      # it 'should ecrypt indicated content - mission affiliation'
-      # it 'should ecrypt indicated content - organization'
-      # it 'should ecrypt indicated content - device'
-      # it 'should ecrypt indicated content - os'
+      it 'should ecrypt indicated content (secret)'
+      it 'should ecrypt indicated content (magenta)'
     end
     context 'with an out-of-band-strategy' do
-      # it 'should reroute indicated content - sensitivity'
-      # it 'should reroute indicated content - category'
-      # it 'should reroute indicated content - mission affiliation'
-      # it 'should reroute indicated content - organization'
-      # it 'should reroute indicated content - device'
-      # it 'should reroute indicated content - os'
+      it 'should reroute indicated content (secret)'
+      it 'should reroute indicated content (magenta)'
     end
   end
   context 'with an artifact without a policy' do
