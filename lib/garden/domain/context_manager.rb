10da9486643d2eb8d27f23e378df2489f235b594
cclamb <chrislambistan@gmail.com>
2012-10-12 13:29:16 -0600

    work

--- lib/garden/domain/context_manager.rb
+++ lib/garden/domain/context_manager.rb
@@ -1,20 +1,20 @@
 
 
 class Garden::Domain::ContextManager
-  
+
 	def context
     { 
       :link => {
         :sensitivity => :secret,
         :category => :magenta,
         :organization => :eurasia,
-        :mission_affiliation => :tropic_thunder
+        :mission_affiliation => :flying_shrub
       },
       :user => {
         :clearance => :top_secret,
         :category => :magenta,
         :organziation => :oceania,
-        :mission_affiliation => :tropic_thunder,
+        :mission_affiliation => :parched_iguana,
         :device => :tablet
       }
     }
