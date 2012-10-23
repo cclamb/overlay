6fc47f34f57a89c00677c32cdaf43fd6118c382c
cclamb <cclamb@cs.unm.edu>
2012-10-22 20:14:55 -0600

    added license information

--- lib/garden.rb
+++ lib/garden.rb
@@ -1,5 +1,17 @@
-# All code (c) 2012, Christopher C. Lamb, 2012
-# All rights reserved
+# Copyright (c) 2012 Christopher C. Lamb
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 # This is a facade of various classes that need to be 
 # included  to run the simulation.  This references
