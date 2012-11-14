6827d217cb984403dbbb395e3505d4550e5c94c8
cclamb <cclamb@cs.unm.edu>
2012-11-14 05:52:28 -0700

    added scratch server for testing

--- bin/server.rb
+++ bin/server.rb
@@ -5,7 +5,9 @@ require 'sinatra/base'
 class Server < Sinatra::Base
 	get '/' do
 		request.env.map{ |e| puts e.to_s + "\n"}
-		return "success!\n"
+		ip_addr = request.env.map['REMOTE_ADDR']
+    hostname = Resolv.new.getname ip_addr
+		return "success! You are #{hostname} and your address is #{ip_addr}.\n"
 	end
 end
 
